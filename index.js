0f3def474b86cd37262f9de0cefe48a3bb58b229
Davide Calarco <s280174@studenti.polito.it>
2021-05-18 15:28:17 +0200

    Start to configure the server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("ciao")
+"use strict"
+
+import express from 'express'
+import mongoose from 'mongoose'
+
+let server = express()
+
+try {
+    await mongoose.connect('mongodb://localhost:27017/Catalogue', { useNewUrlParser: true, useUnifiedTopology: true})
+
+    mongoose.connection.on('error', err => {
+        console.error(`Database connection lost. ${err}`)
+    })
+} catch (error) {
+
+    console.error(`Failed to connect to the database. ${error}`)
+}
+
